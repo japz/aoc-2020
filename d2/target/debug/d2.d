0b9cc073e4b75f48ce0fbfa2c42c12ec8aeb522d
Jasper Capel <jcapel@ebay.com>
2020-12-23 13:09:13 +0100

    add day2 solution

--- d2/target/debug/d2.d
+++
@@ -1,1 +0,0 @@
-/Users/jcapel/proj/aoc/d2/target/debug/d2: /Users/jcapel/proj/aoc/d2/src/main.rs
--- d2/target/debug/deps/d2.d
+++
@@ -1,5 +0,0 @@
-/Users/jcapel/proj/aoc/d2/target/debug/deps/d2: src/main.rs
-
-/Users/jcapel/proj/aoc/d2/target/debug/deps/d2.d: src/main.rs
-
-src/main.rs:
