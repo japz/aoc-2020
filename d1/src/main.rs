f20f9660506fdf00e19d1b70f068e265d6f229fb
Jasper Capel <jcapel@ebay.com>
2020-12-23 13:07:58 +0100

    Update day1 solution

--- d1/src/main.rs
+++ d1/src/main.rs
@@ -24,8 +24,8 @@ fn main() {
 
     let counter = 0;
     'outer: for input1 in input.iter() {
-        for input2 in input_copy.iter() {
-            for input3 in input_copy2.iter() {
+        for input2 in input.iter() {
+            for input3 in input.iter() {
                 if input1 + input2 + input3 == 2020 {
                     println!("{} + {} + {} = 2020! The answer is {}", input1, input2, input3, input1 * input2 * input3);
                     break 'outer;
